e2c547228d86feb778e213ced2ced4c470b0a116
sapunpramuditha <sapunpramuditha14@gmail.com>
2026-05-21 12:00:00 +0530

    add admin database model

---
+++ Backend/models/Admin.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const AdminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please add a username'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+AdminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+AdminSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('Admin', AdminSchema);
